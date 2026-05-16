e7e208f4c9a0ba41e37b6a460cef2f35d81c8926
Mangod4435 <nathanonk29@gmail.com>
2026-05-16 16:06:47 +0700

    Add number formatter

---
+++ Assets/Scripts/NumFormatr.cs
@@ -0,0 +1,136 @@
+using System;
+using UnityEngine;
+
+public class NumFormatr : MonoBehaviour
+{
+    static readonly string[] names =
+    {
+        "",
+        " thousand",
+        " million",
+        " billion",
+        " trillion",
+        " quadrillion",
+        " quintillion",
+        " sextillion",
+        " septillion",
+        " octillion",
+        " nonillion",
+        " decillion",
+        " undecillion",
+        " duodecillion",
+        " tredecillion",
+        " quattuordecillion",
+        " quindecillion",
+        " sexdecillion",
+        " septendecillion",
+        " octodecillion",
+        " novemdecillion",
+        " vigintillion",
+        " unvigintillion",
+        " duovigintillion",
+        " tresvigintillion",
+        " quattuorvigintillion",
+        " quinvigintillion",
+        " sesvigintillion",
+        " septemvigintillion",
+        " octovigintillion",
+        " novemvigintillion",
+        " trigintillion",
+        " untrigintillion",
+        " duotrigintillion",
+        " trestrigintillion",
+        " quattuortrigintillion",
+        " quintrigintillion",
+        " sestrigintillion",
+        " septemtrigintillion",
+        " octotrigintillion",
+        " novemtrigintillion",
+        " quadragintillion",
+        " unquadragintillion",
+        " duoquadragintillion",
+        " tresquadragintillion",
+        " quattuorquadragintillion",
+        " quinquadragintillion",
+        " sesquadragintillion",
+        " septenquadragintillion",
+        " octoquadragintillion",
+        " novemquadragintillion",
+        " quinquagintillion",
+        " unquinquagintillion",
+        " duoquinquagintillion",
+        " tresquinquagintillion",
+        " quattuorquinquagintillion",
+        " quinquinquagintillion",
+        " sesquinquagintillion",
+        " septenquinquagintillion",
+        " octoquinquagintillion",
+        " novemquinquagintillion",
+        " sexagintillion",
+        " unsexagintillion",
+        " duosexagintillion",
+        " tresexagintillion",
+        " quattuorsexagintillion",
+        " quinsexagintillion",
+        " sessexagintillion",
+        " septensexagintillion",
+        " octosexagintillion",
+        " novemsexagintillion",
+        " septuagintillion",
+        " unseptuagintillion",
+        " duoseptuagintillion",
+        " treseptuagintillion",
+        " quattuorseptuagintillion",
+        " quinseptuagintillion",
+        " sesseptuagintillion",
+        " septenseptuagintillion",
+        " octoseptuagintillion",
+        " novemseptuagintillion",
+        " octogintillion",
+        " unoctogintillion",
+        " duooctogintillion",
+        " tresoctogintillion",
+        " quattuoroctogintillion",
+        " quinoctogintillion",
+        " sexoctogintillion",
+        " septemoctogintillion",
+        " octooctogintillion",
+        " novemoctogintillion",
+        " nonagintillion",
+        " unnonagintillion",
+        " duononagintillion",
+        " tresnonagintillion",
+        " quattuornonagintillion",
+        " quinnonagintillion",
+        " sesnonagintillion",
+        " septennonagintillion",
+        " octononagintillion",
+        " novemnonagintillion",
+    };
+
+    static string frmtDob(double n)
+    {
+        if (n > 9.99e+302)
+            return "Infinity";
+
+        if (n < 1000)
+            return n.ToString("0");
+
+        int tier = 0;
+
+        while (n >= 1000 && tier < names.Length - 1)
+        {
+            n /= 1000;
+            tier++;
+        }
+
+        return n.ToString("0.##") + names[tier];
+    }
+
+    private void Start()
+    {
+        double cats = 1e+200;
+        string formattedCats = frmtDob(cats);
+        Debug.Log(formattedCats);
+    }
+}
