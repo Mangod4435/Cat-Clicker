a1a57fcd9e326dba6a5d66725952aa63e5e59b40
Mangod4435 <nathanonk29@gmail.com>
2026-06-03 16:42:20 +0700

    Refactor CatsTextDP script: move to new directory and update structure; add UpgradeDP script as placeholder

---
+++ Assets/Scripts/UI/Text/UpgradeDP.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UpgradeDP : MonoBehaviour
+{
+    [SerializeField]
+    string upgradeName;
+
+    int lastAmount;
+    GameManager instance => GameManager.instance;
+
+    void Update()
+    {
+        // This will be added later
+    }
+}
