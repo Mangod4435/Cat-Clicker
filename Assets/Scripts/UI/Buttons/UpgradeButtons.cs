c95d72255cdeec350b2a08fb5b11e196f036d3a1
Mangod4435 <nathanonk29@gmail.com>
2026-06-06 16:05:04 +0700

    Make an affording system

--- Assets/Scripts/UI/Buttons/UpgradeButtons.cs
+++ Assets/Scripts/UI/Buttons/UpgradeButtons.cs
@@ -1,12 +1,41 @@
 using UnityEngine;
 
-// This is for debugging only for phone i'll do the full one later.
+// This is for debugging only for phone I'll do the full one later.
 
-public class UpgradeButtons : MonoBehaviour
+namespace UI.Buttons
 {
-    [SerializeField]
-    string UpgradeName;
-    GameManager instance => GameManager.instance;
+    public class UpgradeButtons : MonoBehaviour
+    {
+        [SerializeField]
+        string upgradeName;
 
-    public void OnClicked() => instance.AddUpgrade(UpgradeName, 1);
+        private GameManager Instance => GameManager.instance;
+
+        private bool IsAffordable(string localUpgradeName)
+        {
+            switch (localUpgradeName)
+            {
+                case "Cat Food":
+                    switch (Instance.Cats)
+                    {
+                        case < 10:
+                            break;
+                        case >= 10:
+                            return true;
+                    }
+
+                    break;
+            }
+            return false;
+        }
+
+        public void OnClicked()
+        {
+            if (IsAffordable(upgradeName))
+            {
+                Instance.AddUpgrade(upgradeName, 1);
+                Instance.AddCat(-10);
+            }
+        }
+    }
 }
