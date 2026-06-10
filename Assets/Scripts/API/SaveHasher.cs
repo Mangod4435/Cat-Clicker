c76726c2589c263c36a5e27776f496c1a05b912a
Mangod4435 <nathanonk29@gmail.com>
2026-06-10 20:47:15 +0700

    add a simple unfinish function and upgrade gitignore

--- Assets/Scripts/API/SaveHasher.cs
+++ Assets/Scripts/API/SaveHasher.cs
@@ -2,6 +2,10 @@ namespace API
 {
     public class SaveHasher
     {
-        
+        string const key = "word";
+        public static void encode(string json)
+        {
+            
+        }
     }
 }
